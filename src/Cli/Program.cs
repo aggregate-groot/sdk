ab64096b59cd8dcc268bdf3aa2c22210e0ae4849
David Palumbo <david.palumbo@hotmail.com>
2023-07-26 11:21:17 -0400

    Add CLI project.

---
+++ src/Cli/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AggregateGroot.Cli
+{
+    /// <summary>
+    /// Represents the main command line application.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// <param name="args">
+        /// Optional command line arguments.
+        /// </param>
+        private static void Main(string[] args)
+        {         
+            ServiceProvider services = new ServiceCollection()
+                .AddSingleton(PhysicalConsole.Singleton)
+                .BuildServiceProvider();
+
+            CommandLineApplication<RootCommand> application = new ();
+            application.HelpOption("-h|--help");
+
+            application.Conventions
+                .UseDefaultConventions()
+                .UseConstructorInjection(services);
+            
+            application.Execute(args);
+        }
+    }
+}
