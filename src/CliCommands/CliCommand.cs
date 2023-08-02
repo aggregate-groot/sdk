baa08450c4d9f93f3b403515b339797791246c7e
David Palumbo <david.palumbo@hotmail.com>
2023-08-02 00:28:56 -0400

    Add base CLI command types.

---
+++ src/CliCommands/CliCommand.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace AggregateGroot.CliCommands
+{
+    /// <summary>
+    /// Base class for CLI commands.
+    /// </summary>
+    public abstract class CliCommand
+    {
+        /// <summary>
+        /// Runs when this command is executed.
+        /// </summary>
+        /// <param name="application">
+        /// Required command line application.
+        /// </param>
+        [ExcludeFromCodeCoverage]
+        public virtual Task<int> OnExecuteAsync(CommandLineApplication application)
+        {
+            application.ShowHelp();
+            return Task.FromResult(1);
+        }
+    }
+}
---
+++ src/CliCommands/InteractiveCliCommand.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+using McMaster.Extensions.CommandLineUtils;
+
+namespace AggregateGroot.CliCommands
+{
+    /// <summary>
+    /// Base class for interactive CLI commands.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public abstract class InteractiveCliCommand : CliCommand
+    {
+        /// <summary>
+        /// Creates a new instance of the <see cref="InteractiveCliCommand"/> class.
+        /// </summary>
+        /// <param name="console">
+        /// Required type responsible for console input and output.
+        /// </param>
+        /// <param name="prompt">
+        /// Required type responsible for prompting the user for input.
+        /// </param>
+        protected InteractiveCliCommand(
+            IConsole console,
+            IPrompt prompt)
+        {
+            Console = console 
+                ?? throw new ArgumentNullException(nameof(console));
+            Prompt = prompt 
+                ?? throw new ArgumentNullException(nameof(prompt));
+        }
+        
+        /// <summary>
+        /// Gets the console provided for this command.
+        /// </summary>
+        protected IConsole Console { get; }
+        
+        /// <summary>
+        /// Gets the prompt provided for this command.
+        /// </summary>
+        protected IPrompt Prompt { get; }
+    }
+}
