60cba84727e53d5c6a2d392be1e97ac7a9c2cea3
David Palumbo <david.palumbo@hotmail.com>
2023-08-27 15:59:49 -0400

    Add DocFx CLI commands project.

---
+++ src/DocFx.CliCommands/DocFxCliCommand.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+using McMaster.Extensions.CommandLineUtils;
+
+using AggregateGroot.CliCommands;
+
+namespace AggregateGroot.DocFx.CliCommands
+{
+    /// <summary>
+    /// Represents the root for DocFx CLI commands.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    [Command(Name = "docfx", Description = "Root command for working with DocFx.")]
+    public class DocFxCliCommand : CliCommand
+    {
+        
+    }
+}
