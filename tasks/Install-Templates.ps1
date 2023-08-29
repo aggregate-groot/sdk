36eacca192bea0d41465993e09505658611a6c50
David Palumbo <david.palumbo@hotmail.com>
2023-08-28 21:31:26 -0400

    Move template pack out of source path.

--- tasks/Install-Templates.ps1
+++ tasks/Install-Templates.ps1
@@ -1,3 +1,3 @@
 & dotnet new uninstall AggregateGroot.TemplatePack
-& dotnet pack ../src/TemplatePack/TemplatePack.csproj -c Release
-& dotnet new install ../src/TemplatePack/bin/Release/AggregateGroot.TemplatePack.1.0.0.nupkg
+& dotnet pack ../TemplatePack/TemplatePack.csproj -c Release
+& dotnet new install ../TemplatePack/bin/Release/AggregateGroot.TemplatePack.1.0.0.nupkg
