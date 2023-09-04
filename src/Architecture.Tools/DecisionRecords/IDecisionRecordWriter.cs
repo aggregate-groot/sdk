62e0992a75cd7ff300971bff619164f41d2b59d2
David Palumbo <david.palumbo@hotmail.com>
2023-09-04 18:40:10 -0400

    Add architecture tools project.

---
+++ src/Architecture.Tools/DecisionRecords/IDecisionRecordWriter.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+namespace AggregateGroot.Architecture.Tools.DecisionRecords
+{
+    /// <summary>
+    /// Interface for types that can write architecture decision records.
+    /// </summary>
+    public interface IDecisionRecordWriter
+    {
+        /// <summary>
+        /// Writes the provided <paramref name="decisionRecord"/>.
+        /// </summary>
+        /// <param name="decisionRecord">
+        /// Required architecture decision record to write.
+        /// </param>
+        Task WriteDecisionAsync(DecisionRecord decisionRecord);
+    }
+}
