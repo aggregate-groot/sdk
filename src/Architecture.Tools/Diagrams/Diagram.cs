a5235ac5c127d7a7b9ba237895967e357d7bdc4a
David Palumbo <david.palumbo@hotmail.com>
2023-11-12 18:42:48 -0500

    Add file system writer to export an architecture diagram

---
+++ src/Architecture.Tools/Diagrams/Diagram.cs
@@ -0,0 +1,18 @@
+﻿namespace AggregateGroot.Architecture.Tools.Diagrams
+{
+    /// <summary>
+    /// Represents a Structurizr diagram.
+    /// </summary>
+    public record Diagram
+    {
+        /// <summary>
+        /// Gets or initializes the name of the diagram.
+        /// </summary>
+        public required string Name { get; init; }
+
+        /// <summary>
+        /// Gets or initializes the eaw content of the diagram.
+        /// </summary>
+        public required byte[] Content { get; init; }
+    }
+}
---
+++ src/Architecture.Tools/Diagrams/FileSystemDiagramTarget.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AggregateGroot.Architecture.Tools.Diagrams
+{
+    /// <summary>
+    /// File system implementation of the <see cref="IDiagramTarget"/> interface.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public class FileSystemDiagramTarget : IDiagramTarget
+    {
+        /// <inheritdoc />
+        public Task WriteAsync(Diagram diagram, string path)
+        {
+            ArgumentNullException.ThrowIfNull(diagram, nameof(diagram));
+            
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                throw new ArgumentException("Value cannot be null or whitespace.", nameof(path));
+            }
+            
+            string output = Path.Combine(path, diagram.Name);
+            return File.WriteAllBytesAsync($"{output}.png", diagram.Content);
+        }
+    }
+}
