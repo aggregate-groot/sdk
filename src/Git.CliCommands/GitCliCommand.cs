62c1a9526cee85211c8961eb7f83994b681c88d7
David Palumbo <david.palumbo@hotmail.com>
2024-05-05 18:14:08 -0400

    Add git CLI command.

---
+++ src/Git.CliCommands/GitCliCommand.cs
@@ -0,0 +1,14 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+
+using AggregateGroot.CliCommands;
+
+namespace AggregateGroot.Git.CliCommands
+{
+    /// <summary>
+    /// Root command for interacting with Git.
+    /// </summary>
+    [Command("git", Description = "Wraps specialized git commands.")]
+    public class GitCliCommand : CliCommand
+    {
+    }
+}
