38a754079317a96b2596304ed50d8baa1b0669b5
David Palumbo <david.palumbo@hotmail.com>
2024-05-05 17:21:19 -0400

    Add git ignore file representation.

---
+++ src/Git.Tools/GitIgnore/GitIgnoreFile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace AggregateGroot.Git.Tools.GitIgnore
+{
+    /// <summary>
+    /// Represents a .gitignore file
+    /// </summary>
+    public class GitIgnoreFile
+    {
+        /// <summary>
+        /// Creates a new instance of the <see cref="GitIgnoreFile" />.
+        /// </summary>
+        public GitIgnoreFile(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                throw new ArgumentException(
+                    "Value cannot be null or whitespace.", nameof(path));
+            }
+
+            if (!Directory.Exists(path))
+            {
+                throw new DirectoryNotFoundException(
+                    $"The specified path({path}) does not exist.");
+            }
+
+            _path = path;
+        }
+        
+        private readonly string _path;
+    }
+}
---
+++ src/Tests/Git.Tools.Tests.Unit/GitIgnore/GitIgnoreFileTests/WhenConstructing.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+
+using Xunit;
+
+using AggregateGroot.Git.Tools.GitIgnore;
+
+namespace AggregateGroot.Git.Tools.Tests.Unit.GitIgnore.GitIgnoreFileTests
+{
+    /// <summary>
+    /// Specifies the behavior when constructing a <see cref="GitIgnoreFile" />.
+    /// </summary>
+    public class WhenConstructing
+    {
+        /// <summary>
+        /// Tests that passing a null or empty path to the constructor will be rejected.
+        /// </summary>
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        [ExcludeFromCodeCoverage]
+        public void Null_Or_Empty_Path_Is_Rejected(string path)
+        {
+            ArgumentException exception = Assert.Throws<ArgumentException>(
+                () => new GitIgnoreFile(path));
+
+            Assert.Equal("path", exception.ParamName);
+        }
+        
+        /// <summary>
+        /// Tests that passing an invalid path to the constructor will be rejected.
+        /// </summary>
+        [Fact]
+        public void Invalid_Path_Is_Rejected()
+        {
+            const string path = "invalid/path";
+            
+            DirectoryNotFoundException exception = Assert.Throws<DirectoryNotFoundException>(
+                () => new GitIgnoreFile(path));
+            
+            Assert.Contains(path, exception.Message);
+        }
+    }
+}
